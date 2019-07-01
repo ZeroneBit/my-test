73b6f75a7863ad1eaad4dd823d59ec696b461548
ZeroneBit <kaida.zhao@gmail.com>
2019-07-01 16:01:34 +0800

    Updates

---
+++ precache-manifest.fada65985059fd0b6f594281b4809af6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d109d06b1d8280e7437c7b81e4a70dee",
+    "url": "/my-test/index.html"
+  },
+  {
+    "revision": "3ebe0871f855bd49b99d",
+    "url": "/my-test/static/js/2.22246a24.chunk.js"
+  },
+  {
+    "revision": "651963b47ba7a77292db",
+    "url": "/my-test/static/js/main.c5abffcc.chunk.js"
+  },
+  {
+    "revision": "ae26c7dac68064d75c97",
+    "url": "/my-test/static/js/runtime~main.0959f106.js"
+  }
+]);
